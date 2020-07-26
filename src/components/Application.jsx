5f02c29115a4966d87d2b151b841b94bbf9f06c0
hughes <hughes@tumelo.com>
2020-07-26 16:02:16 +0100

    Ability to add post

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { firestore } from "../firebase";
+import { collectIdsAndDocs } from "../utils";
 
 import Posts from "./Posts";
 
@@ -20,37 +21,23 @@ class Application extends Component {
       //   stars: 1,
       //   comments: 47,
       // },
-      // {
-      //   id: "2",
-      //   title: "The Sauciest of Opinions",
-      //   content:
-      //     "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis suscipit repellendus modi unde cumque, fugit in ad necessitatibus eos sed quasi et! Commodi repudiandae tempora ipsum fugiat. Quam, officia excepturi!",
-      //   user: {
-      //     uid: "456",
-      //     displayName: "Mill Burray",
-      //     email: "notbillmurray@mailinator.com",
-      //     photoURL: "https://www.fillmurray.com/400/400",
-      //   },
-      //   stars: 3,
-      //   comments: 0,
-      // },
     ],
   };
 
   async componentDidMount() {
     const snapshots = await firestore.collection("posts").get();
-
-    const posts = snapshots.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
+    const posts = snapshots.docs.map(collectIdsAndDocs);
 
     this.setState({ posts });
   }
 
-  handleCreate = (post) => {
+  handleCreate = async (post) => {
     const { posts } = this.state;
-    this.setState({ posts: [post, ...posts] });
+    const documentReference = await firestore.collection("posts").add(post);
+    const doc = await documentReference.get();
+    const storedPost = collectIdsAndDocs(doc);
+
+    this.setState({ posts: [storedPost, ...posts] });
   };
 
   render() {
