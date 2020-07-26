5d95d808ea701c86cec4b08c7b4082c72448c090
hughes <hughes@tumelo.com>
2020-07-26 13:15:31 +0100

    fetching posts

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -6,42 +6,46 @@ import Posts from "./Posts";
 class Application extends Component {
   state = {
     posts: [
-      {
-        id: "1",
-        title: "A Very Hot Take",
-        content:
-          "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis suscipit repellendus modi unde cumque, fugit in ad necessitatibus eos sed quasi et! Commodi repudiandae tempora ipsum fugiat. Quam, officia excepturi!",
-        user: {
-          uid: "123",
-          displayName: "Bill Murray",
-          email: "billmurray@mailinator.com",
-          photoURL: "https://www.fillmurray.com/300/300",
-        },
-        stars: 1,
-        comments: 47,
-      },
-      {
-        id: "2",
-        title: "The Sauciest of Opinions",
-        content:
-          "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis suscipit repellendus modi unde cumque, fugit in ad necessitatibus eos sed quasi et! Commodi repudiandae tempora ipsum fugiat. Quam, officia excepturi!",
-        user: {
-          uid: "456",
-          displayName: "Mill Burray",
-          email: "notbillmurray@mailinator.com",
-          photoURL: "https://www.fillmurray.com/400/400",
-        },
-        stars: 3,
-        comments: 0,
-      },
+      // {
+      //   id: "1",
+      //   title: "A Very Hot Take",
+      //   content:
+      //     "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis suscipit repellendus modi unde cumque, fugit in ad necessitatibus eos sed quasi et! Commodi repudiandae tempora ipsum fugiat. Quam, officia excepturi!",
+      //   user: {
+      //     uid: "123",
+      //     displayName: "Bill Murray",
+      //     email: "billmurray@mailinator.com",
+      //     photoURL: "https://www.fillmurray.com/300/300",
+      //   },
+      //   stars: 1,
+      //   comments: 47,
+      // },
+      // {
+      //   id: "2",
+      //   title: "The Sauciest of Opinions",
+      //   content:
+      //     "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis suscipit repellendus modi unde cumque, fugit in ad necessitatibus eos sed quasi et! Commodi repudiandae tempora ipsum fugiat. Quam, officia excepturi!",
+      //   user: {
+      //     uid: "456",
+      //     displayName: "Mill Burray",
+      //     email: "notbillmurray@mailinator.com",
+      //     photoURL: "https://www.fillmurray.com/400/400",
+      //   },
+      //   stars: 3,
+      //   comments: 0,
+      // },
     ],
   };
 
-  componentDidMount() {
-    const posts = firestore
-      .collection("users")
-      .get()
-      .then((snapshots) => console.log({ snapshots }));
+  async componentDidMount() {
+    const snapshots = await firestore.collection("posts").get();
+
+    const posts = snapshots.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    this.setState({ posts });
   }
 
   handleCreate = (post) => {
